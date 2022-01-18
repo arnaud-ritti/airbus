1f9de72ba2fa5b2171a9afaf988fa88c960232ba
Elie MAKANY <elie.makany@adeliom.com>
2022-01-19 00:54:31 +0100

    extensions interface

---
+++ extensions/interfaces/airicon/src/index.js
@@ -0,0 +1,11 @@
+import InterfaceComponent from './interface.vue';
+
+export default {
+    id: 'airicon',
+    name: 'Airicon',
+    icon: 'box',
+    description: 'Icon picker for airbus',
+    component: InterfaceComponent,
+    options: null,
+    types: ['string'],
+};
---
+++ extensions/interfaces/airicon/src/interface.vue
@@ -0,0 +1,30 @@
+<template>
+  <input :value="value" @input="handleChange($event.target.value)" />
+  <select name="icon" id="icon">
+    <option value="test">Test</option>
+    <option value="test2">Test2</option>
+  </select>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: null,
+    },
+  },
+  emits: ['input'],
+  setup(props, { emit }) {
+    return { handleChange };
+
+    function handleChange(value) {
+      emit('input', value);
+    }
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
